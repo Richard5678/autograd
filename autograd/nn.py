c969e1119a945bc4d846cbca5b52e60e76f58d3c
Richard5678 <frqrichard@gmail.com>
2025-01-18 18:38:53 -0500

    implemented the Tensor wrapper for tensor data with backward pass that topologically sort computational graph and perform backward propogation for gradient update

---
+++ autograd/nn.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class Tensor:
+    def __init__(self, value, prev=[]):
+        self.value = value
+        self.grad = None
+        self._prev = set(prev)
+
+    def backward(self):
+        """Backward pass
+            - Find computational graph via topological sort
+            - Backpropagation 
+        """
+        
+        # topological sort
+        topo_order = []
+        visited = set()
+
+        def build_topo(node):
+            visited.add(node)
+            for prev in node._prev:
+                if prev not in visited:
+                    build_topo(prev)
+
+            topo_order.append(node)
+            
+        build_topo(self)
+
+        if self.grad == None:
+            self.grad = Tensor(np.ones_like(self.value))
+
+        # back propagatioin
+        for tensor in reversed(topo_order):
+            grads = tensor._backward(tensor.grad)
+            for prev, grad in zip(tensor._prev, grads):
+                if prev.grad == None:
+                    prev.grad = grad
+                else:
+                    prev.grad += grad
+
+
+
+
